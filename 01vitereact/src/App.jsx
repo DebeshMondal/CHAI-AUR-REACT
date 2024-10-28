89fb7767f3d8a2929502882578e10df0393cc0e5
Debesh Mondal <debeshprofessional05@gmail.com>
2024-10-28 09:43:34 +0530

    Refactor App component to import and render Chai component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 
+import chai from "./Chai"
 
 function App() {
  
   return (
-    <h1>Chai aur React with Vite | Debesh Mondal</h1>
+    // <h1>Chai aur React with Vite | Debesh Mondal</h1>
+    <Chai/>
   )
 }
 
