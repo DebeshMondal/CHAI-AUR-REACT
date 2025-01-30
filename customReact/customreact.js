5ebf211ccc50ca2f47a02e4dfab3e55d8de009c9
Debesh Mondal <debeshprofessional05@gmail.com>
2025-01-30 09:52:34 +0530

    Implement customRender function to dynamically create and append DOM elements

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,9 +1,21 @@
+function customRender(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.title)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+} 
+
 const reactElement = {
     type: 'h1',
     props: {
-        title: 'Hello World'
-    }
+        title: 'https://www.google.com',
+        target: 'root'
+    },
+    children: ['Click me!']
 }
 
 const mainContainer = document.querySelector('#root') 
 
+customRender(reactElement, mainContainer) 
