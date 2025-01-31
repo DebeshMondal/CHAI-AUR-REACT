c9403b8e18e3366665c133079feb756140b7f30f
Debesh Mondal <debeshprofessional05@gmail.com>
2025-01-31 10:26:45 +0530

    Refactor customRender function to improve attribute handling and update reactElement structure

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,30 +1,29 @@
-function customRender(reactElement, container){
-    // const domElement = document.createElement(reactElement.type)
-    // domElement.innerHTML = reactElement.children
-    // domElement.setAttribute('href', reactElement.props.title)
-    // domElement.setAttribute('target', reactElement.props.target)
+function customRender(reactElement, container) {
+  // const domElement = document.createElement(reactElement.type)
+  // domElement.innerHTML = reactElement.children
+  // domElement.setAttribute('href', reactElement.props.title)
+  // domElement.setAttribute('target', reactElement.props.target)
 
-    // container.appendChild(domElement)
+  // container.appendChild(domElement)
 
-
-    const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML = reactElement.children
-    for (const prop in reactElement.props) {
-       if (prop === 'children')  continue;
-          domElement.setAttribute(prop, reactElement.props[prop])  
-        }
-    container.appendChild(domElement)
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    if (prop === "children") continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
 }
 
 const reactElement = {
-    type: 'a',
-    props: {
-        title: 'https://www.google.com',
-        target: 'root'
-    },
-    children: ['Click me!']
-}
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  children: ["Click me!"],
+};
 
-const mainContainer = document.querySelector('#root') 
+const mainContainer = document.querySelector("#root");
 
-customRender(reactElement, mainContainer) 
+customRender(reactElement, mainContainer);
