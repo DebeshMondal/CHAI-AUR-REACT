32d4a24a376cb1ddd30e93a8e7eb3c420da4010e
Debesh Mondal <debeshprofessional05@gmail.com>
2025-01-31 10:38:44 +0530

    Add link element to MyApp with external URL and target attributes

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -10,8 +10,18 @@ function MyApp() {
     )
 }
 
+const reactElement = {
+    type: "a",
+    props: {
+      href: "https://www.google.com",
+      target: "_blank",
+    },
+    children: ["Click me!"],
+  };
+
 createRoot(document.getElementById('root')).render(
  
     <MyApp />
+    // MyApp()
    
 )
