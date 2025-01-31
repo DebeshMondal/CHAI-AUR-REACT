5fa376dfaa501c7bba40c8c2f6e3bf0b133fd971
Debesh Mondal <debeshprofessional05@gmail.com>
2025-01-31 10:31:33 +0530

    Replace App component with MyApp to introduce new application structure

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,10 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-
+function MyApp() {
+    return(
+        <div>
+            <h1>My App 2.0</h1>
+        </div>
+    )
+}
 
 createRoot(document.getElementById('root')).render(
  
-    <App />
+    <MyApp />
    
 )
