b08dfa2df9495a1e8ada375c5bcc0dd74f9fc5a4
Debesh Mondal <debeshprofessional05@gmail.com>
2025-04-01 08:35:59 +0530

    Refactor counter implementation in App component to use state and update logic

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,20 +5,24 @@ import './App.css'
 
 function App() {
  
-   let counter = 15  // counter value
+   const [counter,setcount] = useState(15)
+
+   //let counter = 15  // counter value
 
   const addValue = () => {
     // console.log("Add value",Math.random());
 
     counter = counter + 1
+    setcount(counter)
     console.log("Counter value now: ",counter);
+
     
   }
 
   const RemoveValue = () => {
     
     counteer = counter - 1
-    console.log("Counter value now: ",counteer);
+    console.log("Counter value now: ",counter);
     
   }
 
