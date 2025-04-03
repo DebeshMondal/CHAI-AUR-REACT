ab7592b9c7b93ab96d1390866c50812ab26b7cd0
Debesh Mondal <debeshprofessional05@gmail.com>
2025-04-03 20:08:39 +0530

    Pass props to Card component for dynamic content rendering

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,12 +6,20 @@ import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    name: "DeBeSh",
+    age: 22,
+  }
+
+  let myArr = [1,2,3,4,5,6,7,8,9]
 
   return (
     <>
       <h1 className='bg-blue-300 text-red-400 rounded-2xl mb-4'>Tailwind test</h1>
 
-      <Card/>
+      <Card channel="DeBeSh" someObj={myObj}/>
+      <Card channel="HaLuM" newArr={myArr}/>
+      <Card  />
       
     </>
   )
