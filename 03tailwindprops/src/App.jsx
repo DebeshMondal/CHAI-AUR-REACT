0e53189a7fe6822873c6ad703e7ce6514d6806b9
Debesh Mondal <debeshprofessional05@gmail.com>
2025-04-03 10:41:39 +0530

    Enhance App component layout with additional Tailwind CSS styles and new content

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,7 +8,23 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-blue-300'>Tailwind test</h1>
+      <h1 className='bg-blue-300 text-red-400 rounded-2xl'>Tailwind test</h1>
+
+      <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src="https://images.pexels.com/photos/30553472/pexels-photo-30553472/free-photo-of-elderly-couple-relaxing-outdoors-in-mexico.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" />
+  </div>
+  <div class="flex items-center md:items-start">
+    <span class="text-2xl font-medium">Class Warfare</span>
+    <span class="font-medium text-sky-500">The Anti-Patterns</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
+
     </>
   )
 }
