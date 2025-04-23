ea5735d611f53e03f81c7d0033e7449bac6381e0
Debesh Mondal <debeshprofessional05@gmail.com>
2025-04-23 09:33:47 +0530

    Fix casing of className attribute in App component

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl bg-orange-500'>Currency App with Chai</h1>
+      <h1 ClassName='text-3xl bg-orange-500'>Currency App with Chai</h1>
     </>
   )
 }
