ac9af7de82c05d0908bc38938893a92541f0bfd6
Debesh Mondal <debeshprofessional05@gmail.com>
2025-04-25 09:32:42 +0530

    Enhance InputBox component to support amount input and currency selection

--- 06currencyConvertor/src/components/InputBox.jsx
+++ 06currencyConvertor/src/components/InputBox.jsx
@@ -3,22 +3,32 @@ import React from 'react'
 
 function InputBox({
   label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOption = [],
+  currencyType = "usd",
+  amountDisabled = false,
+  currencyDisabled = false,
   
   className = "",
 }) {
  
 
   return (
-      <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+      <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
           <div className="w-1/2">
               <label  className="text-black/40 mb-2 inline-block">
-                  label
+                  {label}
               </label>
               <input
                   
                   className="outline-none w-full bg-transparent py-1.5"
                   type="number"
-                  placeholder="Amount"
+                  placeholder="Amount "
+                  disabled={amountDisabled}
+                  value={amount}
+                  onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
               />
           </div>
           <div className="w-1/2 flex flex-wrap justify-end text-right">
