6049bb27faa9153a61bcca5cf6695c95258a6bd0
Debesh Mondal <debeshprofessional05@gmail.com>
2025-04-27 10:13:24 +0530

    Refactor useCurrencyInfo hook to accept Currency parameter and correctly set fetched data

--- 06currencyConvertor/src/hooks/usecurrencyinfo.js
+++ 06currencyConvertor/src/hooks/usecurrencyinfo.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo() {
+function useCurrencyInfo(Currency) {
   const [data, setData] = useState({});
   useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${Currency}.json`
     )
       .then((response) => response.json())
-      .then((response) => setData(response))
+      .then((response) => setData(response[Currency]))
       .catch((error) => console.error("Error fetching currency info:", error));
   }, []);
   return data;
